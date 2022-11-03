3fe13fed578b23f9f25e7d8370c1c9065d016d2b
solesrodriguez <112837064+solesrodriguez@users.noreply.github.com>
2022-11-03 01:28:43 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+mdc.ripple.MDCRipple.attachTo(document.querySelector('.foo-button'));
+
+import {MDCTopAppBar} from '@material/top-app-bar';
+
+// Instantiation
+const topAppBarElement = document.querySelector('.mdc-top-app-bar');
+const topAppBar = new MDCTopAppBar(topAppBarElement);
